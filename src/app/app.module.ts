adc93de852be719d2a876c5a412b444d8b731712
nurender <nurenderbishnoi@gmail.com>
2018-07-24 19:23:10 +0530

    fetch data from api server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
+import { Route } from '../../node_modules/@angular/compiler/src/core';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
+
+ let routes : Routes = [
+  {
+    path : "login",
+    component: LoginComponent
+  },
+  {
+    path : "posts",
+    component: PostsComponent
+  },
+  {
+    path : "posts/:id",
+    component: PostComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    LoginComponent
+    LoginComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes , {useHash :true })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private ar: ActivatedRoute,
+    private http : HttpClient
+  ) { }
 
-  ngOnInit() {
+  MyFun(form){
+    this.http.post<any>('https://piet-login-app.herokuapp.com/user/login',{
+      email: form.email,
+      password: form.name
+    }).subscribe(res=>{
+      if(res.success){
+        window.alert(res.msg);
+        this.router.navigate(['/dashboard'])
+      } else{
+        window.alert(res.msg)
+      }
+    })
   }
 
 }
